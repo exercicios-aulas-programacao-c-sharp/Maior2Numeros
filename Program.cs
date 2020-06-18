8aa2107604de64870305419ee63fc11977557c69
diego_neri <diego@diegoneri.com>
2020-06-17 23:10:31 -0300

    Finaliza o programa Maior2Numeros

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Maior2Numeros
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal numeroUm, numeroDois, maiorNumero;
+
+            Console.Write("Digite o primeiro número..: ");
+            numeroUm = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite o segundo número...: ");
+            numeroDois = Convert.ToDecimal(Console.ReadLine());            
+
+            if (numeroUm < numeroDois){
+                maiorNumero = numeroDois;
+            }else{
+                maiorNumero = numeroUm;
+            }
+
+            Console.WriteLine($"O maior número é {maiorNumero}");
         }
     }
 }
