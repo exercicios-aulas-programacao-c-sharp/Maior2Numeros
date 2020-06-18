3a4f7edd24992127d7c73efe48432c0e144065f7
diego_neri <diego@diegoneri.com>
2020-06-17 23:04:50 -0300

    Adiciona conteudo apos _dotnet new console_

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
